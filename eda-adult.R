1489ab43c27cdc245d89de161c7730139e7cb1fe
jvpoulos <jvpoulos@gmail.com>
2015-11-04 16:00:49 -0800

    update eda script

--- eda-adult.R
+++ eda-adult.R
@@ -1,9 +1,9 @@
 # Create plots for exploratory data analysis 
 
 # Libraries
-require(ggplot2)
-#require(GGally)
 require(VIM)
+require(ggplot2)
+require(gridExtra)
 
 # Load data from UCI repository
 adult.train <- read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data",
@@ -45,13 +45,42 @@ dev.off()
 # dev.off() 
 
 # Barplot of missing
-pdf("barplot-occ-missing.pdf", width=11.69, height=8.27)
-barMiss(adult.train[c("workclass","occupation","native.country")],
-        pos=2,
-        selection = "any",
-        cex.lab=.6,
-        cex.axis=.5,
-        ylab = "Number of missing/observed in Occupation",
-        xlab = "Occupation")
-dev.off()
-           
+# pdf("barplot-occ-missing.pdf", width=11.69, height=8.27)
+# barMiss(adult.train[c("workclass","occupation","native.country")],
+#         selection = "any",
+#         cex.lab=.6,
+#         cex.axis=.5,
+#         ylab = "Number of missing/observed in Occupation",
+#         xlab = "Occupation")
+# dev.off()
+
+adult.train$workclass.missing <- ifelse(is.na(adult.train$workclass),1,0)
+adult.train$occupation.missing <- ifelse(is.na(adult.train$occupation),1,0)
+adult.train$native.country.missing <- ifelse(is.na(adult.train$native.country),1,0)
+
+workclass.missing <- ggplot(adult.train, aes(x = workclass, fill = workclass.missing)) + 
+  geom_bar(aes(y = (..count..)/sum(..count..))) + 
+  scale_y_continuous(limits=c(0, 1), breaks=c(0,0.25,0.5,0.75,1),labels=c("0", "25", "50", "75", "100")) + 
+  ylab() + 
+  xlab("") +
+  ggtitle("Work class") +
+  theme(legend.position="none") 
+
+occupation.missing <- ggplot(adult.train, aes(x = occupation, fill = occupation.missing)) + 
+  geom_bar(aes(y = (..count..)/sum(..count..))) + 
+  scale_y_continuous(limits=c(0, 1), breaks=c(0,0.25,0.5,0.75,1),labels=c("0", "25", "50", "75", "100")) + 
+  ylab() + 
+  xlab("") +
+  ggtitle("Occupation") +
+  theme(legend.position="none") 
+
+native.country.missing <- ggplot(adult.train, aes(x = native.country, fill = native.country.missing)) + 
+  geom_bar(aes(y = (..count..)/sum(..count..))) + 
+  scale_y_continuous(limits=c(0, 1), breaks=c(0,0.25,0.5,0.75,1),labels=c("0", "25", "50", "75", "100")) + 
+  ylab() + 
+  xlab("") +
+  ggtitle("Native country") +
+  theme(legend.position="none") 
+
+grid.arrange(time.lapse.hist05, time.lapse.hist07,
+             ncol=2, nrow=1, left="Frequency (%)", bottom="")
