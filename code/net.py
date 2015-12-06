65c6ec879e50946708b1355044e4c77be1e7d0c6
jvpoulos <jvpoulos@gmail.com>
2015-12-06 11:51:54 -0800

    changes to nnet

--- code/net.py
+++ code/net.py
@@ -3,7 +3,7 @@
 import theano
 from theano import tensor as T
 import numpy as np
-#from load import mnist
+execfile("load_data.py") # load training and validation sets
 
 def floatX(X):
     return np.asarray(X, dtype=theano.config.floatX)
@@ -11,11 +11,11 @@ def floatX(X):
 def init_weights(shape):
     return theano.shared(floatX(np.random.randn(*shape) * 0.01))
 
-def sgd(cost, params, lr=0.05):
+def sgd(cost, params, gamma):
     grads = T.grad(cost=cost, wrt=params)
     updates = []
     for p, g in zip(params, grads):
-        updates.append([p, p - g * lr])
+        updates.append([p, p - g * gamma])
     return updates
 
 def model(X, w_h, w_o):
@@ -23,27 +23,36 @@ def model(X, w_h, w_o):
     pyx = T.nnet.softmax(T.dot(h, w_o))
     return pyx
 
+# Network topology
+n_inputs = x_train.shape[1]
+n_outputs = len(np.unique(y_train))
+alpha = 2 # arbitrary scaling factor usually 2-10
+n_hidden = (x_train.shape[0])/(alpha*(n_inputs+n_outputs))
 
-#trX, teX, trY, teY = mnist(onehot=True) # trX =training, teX=evaluation
+# Initialize weights
+w_h = init_weights((n_inputs, n_hidden))
+w_o = init_weights((n_hidden, n_outputs))
 
+# Initialize NN classifier
 X = T.fmatrix()
 Y = T.fmatrix()
-
-w_h = init_weights((trX.shape[1], 625))
-w_o = init_weights((625, trY.shape[1]))
 
 py_x = model(X, w_h, w_o)
 y_x = T.argmax(py_x, axis=1)
 
 cost = T.mean(T.nnet.categorical_crossentropy(py_x, Y))
 params = [w_h, w_o]
-updates = sgd(cost, params)
+updates = sgd(cost, params, gamma=10 ** -2)
 
 train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
 predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
 
-for i in range(100):
-    for start, end in zip(range(0, len(trX), 128), range(128, len(trX), 128)): # size of batch
-        cost = train(trX[start:end], trY[start:end])
-    print np.mean(np.argmax(teY, axis=1) == predict(teX))
+# Test on validation set
+n_batches = 128
+max_epoch = 100
 
+for i in range(max_epoch):
+    for start, end in zip(range(0, len(x_train), n_batches), range(n_batches, len(x_train), n_batches)):
+        print x_train[start:end]
+        cost = train(x_train[start:end], y_train[start:end])
+    print np.mean(np.argmax(y_test, axis=1) == predict(x_test))
