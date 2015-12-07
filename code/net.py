224119461be169c54fc6e60d2b91dbd1a6fb5691
Rafael Valle <jrafaelvalle@gmail.com>
2015-12-07 12:15:58 -0800

    automating parameter search

--- code/net.py
+++ code/net.py
@@ -33,32 +33,55 @@ def model(X, w_h, w_o):
 # Network topology
 n_inputs = x_train.shape[1]
 n_outputs = len(np.unique(y_train))
-alpha = 2 # arbitrary scaling factor usually 2-10
-n_hidden = (x_train.shape[0])/(alpha*(n_inputs+n_outputs))
+alphas = np.arange(1, 11) # arbitrary scaling factor usually 2-10
 
-# Initialize weights
-w_h = init_weights((n_inputs, n_hidden))
-w_o = init_weights((n_hidden, n_outputs))
+# dictionary to store results
+results_dict = {}
 
-# Initialize NN classifier
-X = T.fmatrix()
-Y = T.fmatrix()
+for alpha in alphas:
+    n_hidden = (x_train.shape[0])/(alpha*(n_inputs+n_outputs))
 
-py_x = model(X, w_h, w_o)
-y_x = T.argmax(py_x, axis=1)
+    # Initialize weights
+    w_h = init_weights((n_inputs, n_hidden))
+    w_o = init_weights((n_hidden, n_outputs))
 
-cost = T.mean(T.nnet.categorical_crossentropy(py_x, Y))
-params = [w_h, w_o]
-updates = sgd(cost, params, gamma=10 ** -2)
+    # Initialize NN classifier
+    X = T.fmatrix()
+    Y = T.fmatrix()
 
-train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
-predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
+    py_x = model(X, w_h, w_o)
+    y_x = T.argmax(py_x, axis=1)
 
-# Test on validation set
-n_batches = 128
-max_epoch = 10
+    cost = T.mean(T.nnet.categorical_crossentropy(py_x, Y))
+    params = [w_h, w_o]
+    gammas = np.power(10.0, np.arange(-1, -5, -1))
 
-for i in range(max_epoch):
-    for start, end in zip(range(0, len(x_train), n_batches), range(n_batches, len(x_train), n_batches)):
-        cost = train(x_train[start:end], y_train[start:end])
-    print 'epoch {}, accuracy {}, cost {}'.format(i, np.mean(np.argmax(y_test, axis=1) == predict(x_test)), cost)
+    for gamma in gammas:
+        batch_sizes = np.power(2, np.arange(4,14))
+        for batch_size in batch_sizes:
+            model_str = 'alpha {} gamma {} batchsize {}'.format(alpha,
+                                                                gamma,
+                                                                batch_size)
+            updates = sgd(cost, params, gamma=gamma)
+
+            train = theano.function(inputs=[X, Y],
+                                    outputs=cost,
+                                    updates=updates,
+                                    allow_input_downcast=True)
+
+            predict = theano.function(inputs=[X],
+                                      outputs=y_x,
+                                      allow_input_downcast=True)
+
+            # Test on validation set
+            max_epoch = 10
+            print model_str
+            for i in range(max_epoch):
+                for start, end in zip(range(0, len(x_train), batch_size),
+                                      range(batch_size, len(x_train), batch_size)):
+                    test_cost = train(x_train[start:end], y_train[start:end])
+                error_rate = 1 - np.mean(np.argmax(y_test, axis=1) == predict(x_test))
+                print 'epoch {}, error rate {}, cost {}'.format(i,
+                                                                error_rate,
+                                                                test_cost)
+            results_dict[model_str] = (error_rate, cost)
